31bf97d86ca4be2061f8df7ac15edc7be3d4a95e
stedjor <stedjor88@gmail.com>
2020-08-06 02:24:16 +0200

    add git button

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,12 +1,17 @@
 import React, { Component } from 'react'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faGithub } from '@fortawesome/free-brands-svg-icons'
 
 export class Header extends Component {
     render() {
         return (
-            <nav className="navbar navbar-light ">
+            <nav className="navbar navbar-light justify-content-between">
                 <p className="navbar-brand header-signature">
                     Stefan Đorđević
                 </p>
+                <a className="navbar-brand header-icon" target="_blank" href="https://github.com/stedjor/movie-search">
+                    <FontAwesomeIcon icon={faGithub} />
+                </a>
             </nav>
         )
     }
